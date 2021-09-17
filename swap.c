33651d694d2b3e7846629b00cb55fe978fde1b91
rahul <rockzz10rahul@gmail.com>
2021-09-17 12:27:11 +0530

    swap.c

---
+++ swap.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  
+ int main()    
+{    
+int x=30, y=10; 
+    Int temp;     
+    printf("Before swap x=%d y=%d\n",x,y);      
+     x=x+y-(y=x);
+     printf("\nAfter swap x=%d and  y=%d\n",x,y);    
+return 0;  
+}
